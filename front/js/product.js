4b04644b87e60896d4e3db7d2fcd29e9e7e15eb2
Angounou <jcangounou@hotmail.fr>
2023-01-18 16:46:11 +0100

    insertion js

---
+++ front/js/product.js
@@ -0,0 +1,19 @@
+console.log('ca marche et a suivre product bloc');
+const productPhotoArticle = document.querySelector("div.item__content");
+let productImg = document.createElement("img");
+let newProductImg = productPhotoArticle.appendChild(productImg);
+productImg.setAttribute("src", "../images/logo.png");
+productImg.setAttribute("alt","Photographie d'un canapé")
+console.log(newProductImg)
+
+console.log('console suite nom produit');
+const produitNomPrix = document.querySelector("h1#title")
+produitNomPrix.innerHTML = "Nom Produit";
+    console.log(produitNomPrix.innerHTML)
+
+    console.log("console suite produit explique");
+const phraseDescription = document.querySelector("p#description");
+phraseDescription.innerHTML = 'Dis enim malesuada risus sapien gravida nulla nisl arcu.';
+console.log(phraseDescription.innerHTML);
+
+console.log('console  derniere suite pour page product');
--- front/js/script.js
+++ front/js/script.js
@@ -1,17 +1,18 @@
-console.log('test');
 const newDiv = document.querySelector("section#items");
-console.log(newDiv);
-const lienBloc = document.createElement("a");
+    console.log(newDiv);
+let lienBloc = document.createElement("a");
 lienBloc.setAttribute("href", "./product.html?id=42")
-console.log(lienBloc);
-    newDiv.appendChild(lienBloc);
-
+    console.log(lienBloc);
+    
 const articleBloc = document.querySelector("a article");
-const imgArticleBloc = document.createElement("img");
+let imgArticleBloc = document.createElement("img");
 // imgArticleBloc.setAttribute("src", "../product01.jpg");
 imgArticleBloc.setAttribute("alt", "Lorem ipsum dolor sit amet, Kanap name1");
-console.log(imgArticleBloc);
-// const nomProdt = document.
-// // imgArticleBloc.nextElementSibling("h3");
+    console.log(imgArticleBloc);
+const nomProduct = document.createElement("h3");
+nomProduct.classList.add("productName");
+imgArticleBloc.appendChild(nomProduct);
 
+newDiv.appendChild(lienBloc);
+// articleBloc.appendChild(imgArticleBloc);
 
