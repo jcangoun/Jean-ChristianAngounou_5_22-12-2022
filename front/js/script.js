cf9883815c916d6fe304a6780c4d126af6270c3a
Angounou <jcangounou@hotmail.fr>
2023-01-18 14:09:19 +0100

    premier bon commitage

--- front/js/script.js
+++ front/js/script.js
@@ -4,6 +4,14 @@ console.log(newDiv);
 const lienBloc = document.createElement("a");
 lienBloc.setAttribute("href", "./product.html?id=42")
 console.log(lienBloc);
-newDiv.appendChild(lienBloc);
-const imgArticleBloc = document.querySelector("a article");
+    newDiv.appendChild(lienBloc);
+
+const articleBloc = document.querySelector("a article");
+const imgArticleBloc = document.createElement("img");
+// imgArticleBloc.setAttribute("src", "../product01.jpg");
+imgArticleBloc.setAttribute("alt", "Lorem ipsum dolor sit amet, Kanap name1");
 console.log(imgArticleBloc);
+// const nomProdt = document.
+// // imgArticleBloc.nextElementSibling("h3");
+
+
