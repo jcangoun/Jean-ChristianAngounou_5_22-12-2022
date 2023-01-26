46d845e348da60ed90dbd4b9208219ae6dada328
Angounou <jcangounou@hotmail.fr>
2023-01-26 18:04:23 +0100

    petits test api

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,6 @@
 console.log('test')
 
+// crée l'article de classe cart__item d'attribut data-id et aussi data-color
 const carteArticle = document.querySelector('.cart > #cart__items')
 const detailArticl = document.createElement('article')
 detailArticl.classList.add('cart__item')
@@ -7,3 +8,25 @@ detailArticl.setAttribute ('data-id', '{product-ID}"')
 detailArticl.setAttribute ('data-color', '{product-color}')
  carteArticle.append(detailArticl)
  console.log(carteArticle)
+
+// ci dessous crée la dive de class "cart__item__img"
+ const photoArticleCart = document.createElement("div")
+ photoArticleCart.classList.add("cart__item__img")
+ detailArticl.append(photoArticleCart)
+
+//  ci dessous créée l'image enfant avec attribut alt phot canapé et src ../images/product01.jpg
+ const imageCArtItemArtcl = document.createElement("img")
+ imageCArtItemArtcl.setAttribute("alt", "Photographie d'un canapé")
+//  imageCArtItemArtcl.setAttribute("src", "../images/product01.jpg")
+ detailArticl.append(imageCArtItemArtcl)
+ console.log(detailArticl)
+
+ const contenuCartItem = document.createElement('div')
+ contenuCartItem.classList.add("cart__item__content")
+
+ const descriptonContenuCartItem = document.createElement("div")
+ descriptonContenuCartItem.classList.add("cart__item__content__description")
+
+ const nomProduitCarteItem = document.createElement("h2")
+ nomProduitCarteItem.innerHTML = 'Nom du produit'
+
--- front/js/script.js
+++ front/js/script.js
@@ -1,31 +1,31 @@
+// fetch a faire en first line
+
 const newDiv = document.querySelector("section#items");
-    console.log(newDiv);
+console.log(newDiv);
 const lienBloc = document.createElement("a");
-lienBloc.setAttribute("href", "./product.html?id=42")
-    console.log(lienBloc);
-   newDiv.append(lienBloc)
-   const articleDuLienProdt = document.createElement('article')
-   lienBloc.append(articleDuLienProdt)
-   console.log(articleDuLienProdt)
+lienBloc.setAttribute("href", "./product.html?id=42");
+console.log(lienBloc);
+newDiv.append(lienBloc);
+const articleDuLienProdt = document.createElement("article");
+lienBloc.append(articleDuLienProdt);
+console.log(articleDuLienProdt);
 
-
-  const imgArticleBloc = document.createElement("img");
+const imgArticleBloc = document.createElement("img");
 //   Ici il y a un problème avec le lien product 01!!!!!!!!!!!
-
-// imgArticleBloc.setAttribute("src", "../product01.jpg");
+                                    http://localhost:3000/images/kanap01.jpeg
+imgArticleBloc.setAttribute("src", "../product01.jpeg");
 imgArticleBloc.setAttribute("alt", "Lorem ipsum dolor sit amet, Kanap name1");
 
-
-articleDuLienProdt.append(imgArticleBloc)
-console.log(articleDuLienProdt)
+articleDuLienProdt.append(imgArticleBloc);
+console.log(articleDuLienProdt);
 
 const nomProduct = document.createElement("h3");
 nomProduct.classList.add("productName");
 articleDuLienProdt.append(nomProduct);
 
-const phraseCleProdt = document.createElement('p')
-phraseCleProdt.classList.add('productDescription')
-phraseCleProdt.innerHTML= 'Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.'
+const phraseCleProdt = document.createElement("p");
+phraseCleProdt.classList.add("productDescription");
+phraseCleProdt.innerHTML = "Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.";
 
-articleDuLienProdt.append(phraseCleProdt)
-console.log(lienBloc)
+articleDuLienProdt.append(phraseCleProdt);
+console.log(lienBloc);
