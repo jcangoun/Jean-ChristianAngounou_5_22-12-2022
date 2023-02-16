06e74407567d37401a36551f2001900b9a73c654
Angounou <jcangounou@hotmail.fr>
2023-02-16 02:48:50 +0100

    insertion . . products

--- front/js/script.js
+++ front/js/script.js
@@ -5,47 +5,53 @@ fetch("http://localhost:3000/api/products")
       return res.json();
     }
   })
-  .then(function (tablo) {
-    test(tablo);
+  .then(function (products) {
+    articles(products);
   })
   .catch(function (err) {
     // Une erreur est survenue
   });
 
-function test(tablo) {
-  tablo.forEach((element) => {
-    console.log(element.imageUrl);
-    
+function articles(products) {
+  products.forEach((product) => {
+    console.log(product);
+    articlePage(product);
   });
 }
-// je vais pouvoir ajouter un produit mais en haut tu remplaces element par un nom du genre produit 
-const newDiv = document.querySelector("section#items");
-console.log(newDiv);
-const lienBloc = document.createElement("a");
-lienBloc.setAttribute("href", "./product.html?id=42");
-console.log(lienBloc);
-newDiv.append(lienBloc);
-const articleDuLienProdt = document.createElement("article");
-lienBloc.append(articleDuLienProdt);
-console.log(articleDuLienProdt);
 
-const imgArticleBloc = document.createElement("img");
-//   Ici il y a un problème avec le lien product 01 mettre le bon code avec fetch plus tard quand ce sera faisable!!!!!!!!!!!
-// http://localhost:3000/images/kanap01.jpeg
+const articlePage = (product) => {
+  // je vais pouvoir ajouter un produit mais en haut tu remplaces element par un nom du genre produit
+  const newDiv = document.querySelector("section#items");
+  console.log(newDiv);
+  const lienBloc = document.createElement("a");
+  lienBloc.setAttribute("href", "./product.html?id=42");
+  console.log(lienBloc);
+  newDiv.append(lienBloc);
+  const articleDuLienProdt = document.createElement("article");
+  lienBloc.append(articleDuLienProdt);
+  console.log(articleDuLienProdt);
 
-// imgArticleBloc.setAttribute("src", "../product01.jpeg");
-imgArticleBloc.setAttribute("alt", "Lorem ipsum dolor sit amet, Kanap name1");
+  const imgArticleBloc = document.createElement("img");
+  //   Ici il y a un problème avec le lien product 01 mettre le bon code avec fetch plus tard quand ce sera faisable!!!!!!!!!!!
+  // http://localhost:3000/images/kanap01.jpeg
 
-articleDuLienProdt.append(imgArticleBloc);
-console.log(articleDuLienProdt);
+  imgArticleBloc.setAttribute("src", product.imageUrl);
+  imgArticleBloc.setAttribute("alt", product.altTxt);
 
-const nomProduct = document.createElement("h3");
-nomProduct.classList.add("productName");
-articleDuLienProdt.append(nomProduct);
+  articleDuLienProdt.append(imgArticleBloc);
+  console.log(articleDuLienProdt);
 
-const phraseCleProdt = document.createElement("p");
-phraseCleProdt.classList.add("productDescription");
-phraseCleProdt.innerHTML = "Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.";
+  const nomProduct = document.createElement("h3");
+  nomProduct.classList.add("productName");
+  articleDuLienProdt.append(nomProduct);
 
-articleDuLienProdt.append(phraseCleProdt);
-console.log(lienBloc);
+  const phraseCleProdt = document.createElement("p");
+  phraseCleProdt.classList.add("productDescription");
+  phraseCleProdt.innerHTML = product.description;
+
+  articleDuLienProdt.append(phraseCleProdt);
+  console.log(lienBloc);
+};
+
+
+// Ajouter titre qui a ete oublie
