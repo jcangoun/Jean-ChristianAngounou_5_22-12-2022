41a9aa3fc3b847de8d042892a740d19cef4e95bb
Angounou <jcangounou@hotmail.fr>
2023-03-07 11:53:30 +0100

    toutes les petites modifications de la semaine passée

--- front/js/product.js
+++ front/js/product.js
@@ -1,53 +1,64 @@
-    console.log('ca marche et a suivre product bloc')
+console.log("ca marche et a suivre product bloc");
 
-    // utiliser searchParams pour recupere l'id d'un produit dans l'url
-// fetch("http://localhost:3000/api/products")
-//     .then (function(res) {
-//         if (res.ok)
-//         return res.json();
-//     }
-    
-//     )
+// var tesse = "https://waytolearnx.com/t.html?name=alex-babtise&age=25&address=paris";
+// var url = new URL(tesse);
+// const idhet = url.searchParams.get("id");
+// // console.log("test window Location:", window.location);
+const info = window.location.search;
+
+// console.log("window Location:", window.location);
+const urlParams = new URLSearchParams(info);
+
+// const general = urlParams.get(info);
+
+// const ParamAltTxt = urlParams.get("altTxt");
+// const ParamColors = urlParams.get("colors");
+// const ParamDexcription = urlParams.get("description");
+// const ParamimageUrl = urlParams.get("imageUrl");
+// const ParamName = urlParams.get("name");
+// const ParamPrice = urlParams.get("price");
+const paramId = urlParams.get("id");
+console.log(paramId);
+// utiliser searchParams pour recupere l'id d'un produit dans l'url
+fetch(`http://localhost:3000/api/products/${paramId}`).then(function (res) {
+  if (res.ok) return res.json();
+});
 
 let b = document.main;
-const productPhotoArticle = document.querySelector("div.item__img")
-let productImg = document.createElement("img")
-let newProductImg = productPhotoArticle.append(productImg)
-productImg.setAttribute("src", "../images/logo.png")
-productImg.setAttribute("alt","Photographie d'un canapé")
-    // console.log(newProductImg)
-    // console.log(b)
-
+const productPhotoArticle = document.querySelector("div.item__img");
+let productImg = document.createElement("img");
+let newProductImg = productPhotoArticle.append(productImg);
+productImg.setAttribute("src", "../images/logo.png");
+productImg.setAttribute("alt", "Photographie d'un canapé");
+// console.log(newProductImg)
+// console.log(b)
 
 // console.log('console suite nom produit');
-const produitNomPrix = document.querySelector("h1#title")
-produitNomPrix.innerHTML = "Nom Produit"
-    // console.log(produitNomPrix)
+const produitNomPrix = document.querySelector("h1#title");
+produitNomPrix.innerHTML = "Nom Produit";
+// console.log(produitNomPrix)
 
+const valeurPrix = (document.querySelector("span#price").innerHTML = "42");
+// console.log(valeurPrix)
 
-const valeurPrix = document.querySelector("span#price").innerHTML = '42';
-    // console.log(valeurPrix)
-
-    // console.log("console suite produit explique")
-const phraseDescription = document.querySelector("p#description")
-phraseDescription.innerHTML = 'Dis enim malesuada risus sapien gravida nulla nisl arcu.';
-    // console.log(phraseDescription)
-
+// console.log("console suite produit explique")
+const phraseDescription = document.querySelector("p#description");
+phraseDescription.innerHTML = "Dis enim malesuada risus sapien gravida nulla nisl arcu.";
+// console.log(phraseDescription)
 
 // console.log('console  derniere suite pour page product');
 
 const firstSelectForm = document.querySelector("#colors");
-    // console.log(firstSelectForm)
-const premiereOptionValue = document.createElement('option');
-premiereOptionValue.setAttribute("value", "vert")
-premiereOptionValue.innerHTML = 'vert';
-firstSelectForm.append(premiereOptionValue)
+// console.log(firstSelectForm)
+const premiereOptionValue = document.createElement("option");
+premiereOptionValue.setAttribute("value", "vert");
+premiereOptionValue.innerHTML = "vert";
+firstSelectForm.append(premiereOptionValue);
 
+const deuxiemeOptionValue = document.createElement("option");
+deuxiemeOptionValue.setAttribute("value", "blanc");
+deuxiemeOptionValue.innerHTML = "blanc";
+firstSelectForm.append(deuxiemeOptionValue);
 
-const deuxiemeOptionValue = document.createElement('option')
-deuxiemeOptionValue.setAttribute('value', 'blanc')
-deuxiemeOptionValue.innerHTML = 'blanc'
-firstSelectForm.append(deuxiemeOptionValue)
-
-    // console.log(firstSelectForm)
-    // console.log(b)
+// console.log(firstSelectForm)
+// console.log(b)
--- front/js/script.js
+++ front/js/script.js
@@ -20,18 +20,18 @@ function articles(products) {
 }
 
 const articlePage = (product) => {
-
-  // chargement du noeud section ou on va creer la page 
+  const { _id, imageUrl, altTxt, name, description, price } = product;
+  // chargement du noeud section ou on va creer la page
   const newDiv = document.querySelector("section#items");
 
   // creation du noe <a> pas d'attributs
   const lienBloc = document.createElement("a");
   //  a revoir
-  lienBloc.setAttribute("href", "./product.html?id=_id");
-  // console.log(lienBloc);
+  lienBloc.setAttribute("href", `./product.html?id=${_id}`);
+  console.log(lienBloc);
   newDiv.append(lienBloc);
 
-// creation du noeud <article>, pas attributs
+  // creation du noeud <article>, pas attributs
   const articleDuLienProdt = document.createElement("article");
   // ci dessous ajout de <img>
   lienBloc.append(articleDuLienProdt);
@@ -39,16 +39,15 @@ const articlePage = (product) => {
 
   // Noeud img nommé product.img
   const imgArticleBloc = document.createElement("img");
-  imgArticleBloc.setAttribute("src", product.imageUrl);
-  imgArticleBloc.setAttribute("alt", product.altTxt);
+  imgArticleBloc.setAttribute("src", imageUrl);
+  imgArticleBloc.setAttribute("alt", altTxt);
   // console.log(articleDuLienProdt);
 
-  
-//noeud <h3> class product.name
+  //noeud <h3> class product.name
   const nomProduct = document.createElement("h3");
   nomProduct.classList.add("productName");
-  nomProduct.innerHTML = product.name;
-  
+  nomProduct.innerHTML = name;
+
   // Ajout des noeuds en
   articleDuLienProdt.append(imgArticleBloc, nomProduct);
   // document.querySelector('.productName').innerText = product.name;
@@ -56,7 +55,7 @@ const articlePage = (product) => {
   //la description du canape en bas
   const phraseCleProdt = document.createElement("p");
   phraseCleProdt.classList.add("productDescription");
-  phraseCleProdt.innerText = product.description;
+  phraseCleProdt.innerText = description;
   articleDuLienProdt.append(phraseCleProdt);
   // console.log(lienBloc);
 };
