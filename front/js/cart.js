8f9d4cd507d15b75fe6553ebc9aca052f2d16282
Angounou <jcangounou@hotmail.fr>
2023-09-12 12:12:50 +0200

    debug sur accolade fetch

--- front/js/cart.js
+++ front/js/cart.js
@@ -262,200 +262,200 @@ const fetchEtVisualSection = async () => {
   
   // tous les éléments de l'user a envoyer au serveur
   
-  // Affichage de toutes les procedures incluses quand on appele le fetch
-  fetchEtVisualSection();
-  
-      const form = document.querySelector("#order");
-      
-      const prenom = document.querySelector("#firstName").value;
-      prenom;
-      const prenomError = document.getElementById("firstNameErrorMsg");
-  
-      const nom = document.querySelector("#lastName").value;
-      nom;
-      const nomError = document.getElementById("lastNameErrorMsg");
-  
-      const adresse = document.querySelector("#address").value;
-      const addresseError = document.getElementById("addressErrorMsg");
-  
-      const ville = document.querySelector("#city").value;
-      ville;
-      const villeError = document.querySelector("#city") 
-      
-      const email = document.querySelector("#email").value;
-      email;
-      const emailError = document.querySelector("#email")
-  
-      // const prenomEcout = document.querySelector("#firstName")
-      // prenomEcout.addEventListener ( 'change', function(e){
-      //   const onEcout = e.target.value
-      //   console.log(onEcout)
-      // })
-  
-      // A REMETTRE PLUS TARD EN BAS DANS LE BOUTON
-  
-      // a REMETTRE PLUS TARD L0 JUSTE EN BAS DANS LE BOUTON
-  
-      function boutonPanierComander() {
-        const commanderBtn = document.querySelector("#order");
-        commanderBtn.addEventListener("click", function (e) {
-          // console.log('ca paniasse');
-          e.preventDefault;
-  
-          // valeurs du formulaire qui seront sauvegardés dans localstorage key contact
-          const contact = {
-            prenom: document.querySelector("#firstName"),
-            nom: document.querySelector("#lastName"),
-            addresse: document.querySelector("#address"),
-            ville: document.querySelector("#city"),
-            email: document.querySelector("#email"),          
-          };
-  
-          let prenomValue = document.querySelector("#firstName").value;
-          let nomValue = document.querySelector("#lastName").value;
-          let addresseValue = document.querySelector("#address").value;
-          let villeValue = document.querySelector("#city").value;
-          let emailValue = document.querySelector("#email").value;
-  
-          console.log("aV", addresseValue)
-          console.log("vV", villeValue)
-          console.log("eV", emailValue)
-          // C 'est  ici qu on remmetrtrra les infos formulaires
-          // ----------------------------------------------------------       
-  
-          // localStorage.setItem("contact", JSON.stringify(contact))
-  
-        // là je vais tester les funstions pour les input regex ------------------------------------------------------
-       
-        // PRENOM
-        const regexPrenomValue = /^[A-Za-z][a-z]+(([\ ])?[\s\-\_]([\ ])?[A-Za-z][a-z]+)+/;
-        // regexPrenomValue.test(prenomValue)
-        console.log(regexPrenomValue.test(prenomValue))
-        if (regexPrenomValue.test(prenomValue)) {
-          return prenomValue;
-        } else {
-          prenomError.textContent = "Vérifiez le prénom vous avez sûrement mis des chiffres ou symboles. ";
-        };
-  
-        // NOM
-        const regexNomValue = /[A-Za-z][a-z]+([\s\-\_][A-Za-z][a-z]+)?/;
-        console.log(regexNomValue.test(nomValue));
-        if (regexNomValue.test(nomValue)) {
-          if (nomError.textContent !== null) {
-            nomError.textContent = ""
-            console.log(nomError.textContent)
-          } else {
-            console.log('nomerror est dejà vide donc ce <p> ne se fera pas' )
-          }
-          return nomValue;
-        } else {
-          nomError.textContent = "Vérifiez le nom vous avez sûrement mis des chiffres ou symboles. ";
-        };
-  
-          // function verifNom(nomValue) {
-          //   console.log("nom", "+", nomValue)
-          //   const regexNomValue = /[A-Za-z][a-z]+([\s\-\_][A-Za-z][a-z]+)?/;
-          //   console.log(regexNomValue.test(nomValue));
-          //   if (regexNomValue.test(nomValue)) {
-          //     return nomValue;
-          //   } else {
-          //     nomError.textContent = "Vérifiez le nom vous avez sûrement mis des chiffres ou symboles. ";
-          //   }
-          // }
-          // verifNom();
-  
-          // Je créé une fonction la partie du form qui n'acceptera pas de chiffres
-          function addresseFrValide(addresseValue) {
-            console.log("addresse", "+", addresseValue)
-            const regexAddresseValue = /(^[0-9]{2,3}[\,\s])?([0-9a-zA-Z])?[a-zA-Z0-9\s\-\_]+([\,\s])?(\s)+?([A-Za-z])?[a-zA-Z0-9]+(((\s)?[\-]{1,2}(\s)?([A-Za-z])[a-zA-Z0-9]+){2,5})?(([\ ])?([\-\_\ ])?([\ ])?)?[0-9]{2,5}/gm;
-            console.log(regexAddresseValue.test(addresseValue));
-            if (regexAddresseValue.test(addresseValue)) {
-              return addresseValue;
-            } else {
-              addresseError.textContent = "Vérifiez que votre adresse est bien écrite au format d'addresses francaises"
-            }
-          }
-          addresseFrValide();
-  
-          // function addresseFrValide(addresseValue) {
-          //   console.log("addresse", "+", addresseValue)
-          //   const regexAddresseValue = /(^[0-9]{2,3}[\,\s])?([0-9a-zA-Z])?[a-zA-Z0-9\s\-\_]+([\,\s])?(\s)+?([A-Za-z])?[a-zA-Z0-9]+(((\s)?[\-]{1,2}(\s)?([A-Za-z])[a-zA-Z0-9]+){2,5})?(([\ ])?([\-\_\ ])?([\ ])?)?[0-9]{2,5}/gm;
-          //   console.log(regexAddresseValue.test(addresseValue));
-          //   if (regexAddresseValue.test(addresseValue)) {
-          //     return addresseValue;
-          //   } else {
-          //     addresseError.textContent = "Vérifiez que votre adresse est bien écrite au format d'addresses francaises"
-          //   }
-          // }
-          // addresseFrValide();
-  
-          function villeValide (villeValue) {
-            console.log("ville + ",villeValue )
-            const regexVilleValue = /^[A-Z][a-z]+(([\-\ ])(([A-Z])?[a-z]+))+/g
-            console.log(regexVilleValue.test(villeValue))
-            if (regexEmailValue.test(villeValue)) {
-              return villeValue;
-            } else {
-              villeError.textContent = "Vérifiez que votre ou vos noms de la ville débutent par une majuscule"
-            }
-          }
-  
-          function aLeMail(emailValue) {
-            console.log("email", "+", emailValue)
-            const regexEmailValue = /^[a-zA-Z0-9][a-zA-Z0-9]+[A-Za-z0-9]+(([\ \-\_])?([\-\_\.])?([\ \-\_])?)?([A-Za-z0-9])?[a-zA-Z0-9]+[@][a-z]+[\.][a-z]{2,3}/g;
-            console.log(regexEmailValue.test(emailValue));
-            if (regexEmailValue.test(emailValue)) {
-              return emailValue;
-            } else {
-              emailError.textContent = "Vérifiez que votre email est bien écrite au format email"
-            }
-          }
-          aLeMail();
-  
-          // exemplaire en function des logiques regex 
-  
-          // function aLeMail(emailValue) {
-          //   console.log("email", "+", emailValue)
-          //   const regexEmailValue = /^[a-zA-Z0-9][a-zA-Z0-9]+[A-Za-z0-9]+(([\ \-\_])?([\-\_\.])?([\ \-\_])?)?([A-Za-z0-9])?[a-zA-Z0-9]+[@][a-z]+[\.][a-z]{2,3}/g;
-          //   console.log(regexEmailValue.test(emailValue));
-          //   if (regexEmailValue.test(emailValue)) {
-          //     return emailValue;
-          //   } else {
-          //     emailError.textContent = "Vérifiez que votre email est bien écrite au format email"
-          //   }
-          // }
-          // aLeMail();
-          
-          function aSymbole(value) {
-            return /[^a-zA-Z0-9_]/g.test(value);
-          }
-          aSymbole();
+};
+// Affichage de toutes les procedures incluses quand on appele le fetch
+fetchEtVisualSection();
 
-          const prenomValide = regexPrenomValue.test(prenomValue);
-          const nomValide = regexNomValue.test(nomValue);
-          const adresseValide = regexAddresseValue.test(addresseValue);
-          const lavilleValide = regexVilleValue.test(villeValue);
-          const emailValide =      regexEmailValue.test(emailValue)  ;   
-          const formValide = {
-            prenomValide,
-            nomValide,
-            adresseValide,
-            lavilleValide,
-            emailValide,      
+    const form = document.querySelector("#order");
+    
+    const prenom = document.querySelector("#firstName").value;
+    prenom;
+    const prenomError = document.getElementById("firstNameErrorMsg");
+
+    const nom = document.querySelector("#lastName").value;
+    nom;
+    const nomError = document.getElementById("lastNameErrorMsg");
+
+    const adresse = document.querySelector("#address").value;
+    const addresseError = document.getElementById("addressErrorMsg");
+
+    const ville = document.querySelector("#city").value;
+    ville;
+    const villeError = document.querySelector("#city") 
+    
+    const email = document.querySelector("#email").value;
+    email;
+    const emailError = document.querySelector("#email")
+
+    // const prenomEcout = document.querySelector("#firstName")
+    // prenomEcout.addEventListener ( 'change', function(e){
+    //   const onEcout = e.target.value
+    //   console.log(onEcout)
+    // })
+
+    // A REMETTRE PLUS TARD EN BAS DANS LE BOUTON
+
+    // a REMETTRE PLUS TARD L0 JUSTE EN BAS DANS LE BOUTON
+
+    function boutonPanierComander() {
+      const commanderBtn = document.querySelector("#order");
+      commanderBtn.addEventListener("click", function (e) {
+        // console.log('ca paniasse');
+        e.preventDefault;
+
+        // valeurs du formulaire qui seront sauvegardés dans localstorage key contact
+        const contact = {
+          prenom: document.querySelector("#firstName"),
+          nom: document.querySelector("#lastName"),
+          addresse: document.querySelector("#address"),
+          ville: document.querySelector("#city"),
+          email: document.querySelector("#email"),          
+        };
+
+        let prenomValue = document.querySelector("#firstName").value;
+        let nomValue = document.querySelector("#lastName").value;
+        let addresseValue = document.querySelector("#address").value;
+        let villeValue = document.querySelector("#city").value;
+        let emailValue = document.querySelector("#email").value;
+
+        console.log("aV", addresseValue)
+        console.log("vV", villeValue)
+        console.log("eV", emailValue)
+        // C 'est  ici qu on remmetrtrra les infos formulaires
+        // ----------------------------------------------------------       
+
+        // localStorage.setItem("contact", JSON.stringify(contact))
+
+      // là je vais tester les funstions pour les input regex ------------------------------------------------------
+     
+      // PRENOM
+      const regexPrenomValue = /^[A-Za-z][a-z]+(([\ ])?[\s\-\_]([\ ])?[A-Za-z][a-z]+)+/;
+      // regexPrenomValue.test(prenomValue)
+      console.log(regexPrenomValue.test(prenomValue))
+      if (regexPrenomValue.test(prenomValue)) {
+        return prenomValue;
+      } else {
+        prenomError.textContent = "Vérifiez le prénom vous avez sûrement mis des chiffres ou symboles. ";
+      };
+
+      // NOM
+      const regexNomValue = /[A-Za-z][a-z]+([\s\-\_][A-Za-z][a-z]+)?/;
+      console.log(regexNomValue.test(nomValue));
+      if (regexNomValue.test(nomValue)) {
+        if (nomError.textContent !== null) {
+          nomError.textContent = ""
+          console.log(nomError.textContent)
+        } else {
+          console.log('nomerror est dejà vide donc ce <p> ne se fera pas' )
+        }
+        return nomValue;
+      } else {
+        nomError.textContent = "Vérifiez le nom vous avez sûrement mis des chiffres ou symboles. ";
+      };
+
+        // function verifNom(nomValue) {
+        //   console.log("nom", "+", nomValue)
+        //   const regexNomValue = /[A-Za-z][a-z]+([\s\-\_][A-Za-z][a-z]+)?/;
+        //   console.log(regexNomValue.test(nomValue));
+        //   if (regexNomValue.test(nomValue)) {
+        //     return nomValue;
+        //   } else {
+        //     nomError.textContent = "Vérifiez le nom vous avez sûrement mis des chiffres ou symboles. ";
+        //   }
+        // }
+        // verifNom();
+
+        // Je créé une fonction la partie du form qui n'acceptera pas de chiffres
+        function addresseFrValide(addresseValue) {
+          console.log("addresse", "+", addresseValue)
+          const regexAddresseValue = /(^[0-9]{2,3}[\,\s])?([0-9a-zA-Z])?[a-zA-Z0-9\s\-\_]+([\,\s])?(\s)+?([A-Za-z])?[a-zA-Z0-9]+(((\s)?[\-]{1,2}(\s)?([A-Za-z])[a-zA-Z0-9]+){2,5})?(([\ ])?([\-\_\ ])?([\ ])?)?[0-9]{2,5}/gm;
+          console.log(regexAddresseValue.test(addresseValue));
+          if (regexAddresseValue.test(addresseValue)) {
+            return addresseValue;
+          } else {
+            addresseError.textContent = "Vérifiez que votre adresse est bien écrite au format d'addresses francaises"
           }
-          // Ici en bas je peux regrouper les objets que je dois transmettre
-          const userFormToSend = {
-            cartPanierGet,
-            contact,
-          };
-          console.log("userFormToSend");
-          console.log(userFormToSend);
-  
-          // ICi en bas on va mettre des conditions aavnt de permettre le localSotrage .
-          // localStorage.setItem("contact", JSON.stringify(contact))
-  
-          // const formulaireValide =
-        });
-      }
-      boutonPanierComander();
-  };
+        }
+        addresseFrValide();
+
+        // function addresseFrValide(addresseValue) {
+        //   console.log("addresse", "+", addresseValue)
+        //   const regexAddresseValue = /(^[0-9]{2,3}[\,\s])?([0-9a-zA-Z])?[a-zA-Z0-9\s\-\_]+([\,\s])?(\s)+?([A-Za-z])?[a-zA-Z0-9]+(((\s)?[\-]{1,2}(\s)?([A-Za-z])[a-zA-Z0-9]+){2,5})?(([\ ])?([\-\_\ ])?([\ ])?)?[0-9]{2,5}/gm;
+        //   console.log(regexAddresseValue.test(addresseValue));
+        //   if (regexAddresseValue.test(addresseValue)) {
+        //     return addresseValue;
+        //   } else {
+        //     addresseError.textContent = "Vérifiez que votre adresse est bien écrite au format d'addresses francaises"
+        //   }
+        // }
+        // addresseFrValide();
+
+        function villeValide (villeValue) {
+          console.log("ville + ",villeValue )
+          const regexVilleValue = /^[A-Z][a-z]+(([\-\ ])(([A-Z])?[a-z]+))+/g
+          console.log(regexVilleValue.test(villeValue))
+          if (regexEmailValue.test(villeValue)) {
+            return villeValue;
+          } else {
+            villeError.textContent = "Vérifiez que votre ou vos noms de la ville débutent par une majuscule"
+          }
+        }
+
+        function aLeMail(emailValue) {
+          console.log("email", "+", emailValue)
+          const regexEmailValue = /^[a-zA-Z0-9][a-zA-Z0-9]+[A-Za-z0-9]+(([\ \-\_])?([\-\_\.])?([\ \-\_])?)?([A-Za-z0-9])?[a-zA-Z0-9]+[@][a-z]+[\.][a-z]{2,3}/g;
+          console.log(regexEmailValue.test(emailValue));
+          if (regexEmailValue.test(emailValue)) {
+            return emailValue;
+          } else {
+            emailError.textContent = "Vérifiez que votre email est bien écrite au format email"
+          }
+        }
+        aLeMail();
+
+        // exemplaire en function des logiques regex 
+
+        // function aLeMail(emailValue) {
+        //   console.log("email", "+", emailValue)
+        //   const regexEmailValue = /^[a-zA-Z0-9][a-zA-Z0-9]+[A-Za-z0-9]+(([\ \-\_])?([\-\_\.])?([\ \-\_])?)?([A-Za-z0-9])?[a-zA-Z0-9]+[@][a-z]+[\.][a-z]{2,3}/g;
+        //   console.log(regexEmailValue.test(emailValue));
+        //   if (regexEmailValue.test(emailValue)) {
+        //     return emailValue;
+        //   } else {
+        //     emailError.textContent = "Vérifiez que votre email est bien écrite au format email"
+        //   }
+        // }
+        // aLeMail();
+        
+        function aSymbole(value) {
+          return /[^a-zA-Z0-9_]/g.test(value);
+        }
+        aSymbole();
+
+        const prenomValide = regexPrenomValue.test(prenomValue);
+        const nomValide = regexNomValue.test(nomValue);
+        const adresseValide = regexAddresseValue.test(addresseValue);
+        const lavilleValide = regexVilleValue.test(villeValue);
+        const emailValide =      regexEmailValue.test(emailValue)  ;   
+        const formValide = {
+          prenomValide,
+          nomValide,
+          adresseValide,
+          lavilleValide,
+          emailValide,      
+        }
+        // Ici en bas je peux regrouper les objets que je dois transmettre
+        const userFormToSend = {
+          cartPanierGet,
+          contact,
+        };
+        console.log("userFormToSend");
+        console.log(userFormToSend);
+
+        // ICi en bas on va mettre des conditions aavnt de permettre le localSotrage .
+        // localStorage.setItem("contact", JSON.stringify(contact))
+
+        // const formulaireValide =
+      });
+    }
+    boutonPanierComander();
