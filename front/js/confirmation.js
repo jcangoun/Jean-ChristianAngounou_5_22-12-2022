45c129bed0939a7d23c26a539779236862648bfd
Angounou <jcangounou@hotmail.fr>
2023-11-20 17:01:00 +0100

    page confirmation finie

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,13 +1,17 @@
 const info = window.location.search;
-console.log(info)
+// console.log(info)
 const urlConfirmParam = new URLSearchParams(info);
-console.log("id de l'order dans l'url", urlConfirmParam);
+// console.log("id de l'order dans l'url", urlConfirmParam);
 
 const orderIdparams = urlConfirmParam.get("orderId");
-console.log(orderIdparams);
+// console.log(orderIdparams);
 
 const caseNumeroCommande = document.getElementById("orderId");
-console.log(caseNumeroCommande)
+// console.log(caseNumeroCommande)
 
-caseNumeroCommande.innerHTML = orderIdparams
-console.log(caseNumeroCommande.textContent)
+caseNumeroCommande.textContent = orderIdparams
+
+// console.log(caseNumeroCommande.textContent)
+
+//  Effacer le storage du panier
+localStorage.removeItem('panier');
